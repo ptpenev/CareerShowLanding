3991f1c2954a6e8766f0512090321f34f043e7bf
ptpenev <ptpenev@diadraw.com>
2025-08-27 10:08:55 +0300

    Commit changes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CareerShowLanding/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
